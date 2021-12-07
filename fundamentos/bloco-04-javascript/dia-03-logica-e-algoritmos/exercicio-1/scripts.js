db91a4a86e091d51244d6aa4e3bfcae470107408
ziraelGustavo <ziraelgustavo@gmail.com>
2021-12-07 20:12:15 -0300

    exercicios de logica e algoritmos' ' '

---
+++ fundamentos/bloco-04-javascript/dia-03-logica-e-algoritmos/exercicio-1/scripts.js
@@ -0,0 +1,51 @@
+// let fatorial = 1;
+
+// for (let index = 10; index > 0; index -= 1) {
+//   fatorial *= index;
+// }
+
+// console.log(fatorial);
+
+// let word = 'eae mano, tudo bom';
+
+// let invertedWord = "";
+
+// for (let i = 0; i < word.length; i += 1) {
+//     invertedWord += word[word.length - 1 - i]
+// } console.log(invertedWord)
+
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+// let biggestWord = array[0];
+// let smallestWord = array[0];
+
+// for (let index = 0; index < array.length; index += 1) {
+//   if (array[index].length > biggestWord.length) {
+//     biggestWord = array[index];
+//   }
+// }
+
+// for (let index = 0; index < array.length; index += 1) {
+//   if (array[index].length < smallestWord.length) {
+//     smallestWord = array[index];
+//   }
+// }
+
+// console.log(biggestWord);
+// console.log(smallestWord);
+
+let biggestPrimeNumber = 0;
+
+for (let currentNumber = 0; currentNumber <= 50; currentNumber += 1) {
+  let isPrime = true;
+  for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+    if (currentNumber % currentDivisor === 0) {
+      isPrime = false;
+    }
+  }
+  if (isPrime) {
+    biggestPrimeNumber = currentNumber;
+  }
+}
+
+console.log(biggestPrimeNumber);
